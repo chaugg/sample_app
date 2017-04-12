d0ee33b909e8472a95eb006eae7affc5d11674ec
Chau Giang <tothelegendary@gmail.com>
2017-04-12 08:30:43 +0700

    Chap5 finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+
   scope "(:locale)", :locale => /vn|en/ do
     get "static_pages/home"
     get "/help", to: "static_pages#help"
     get "/about", to: "static_pages#about"
+    get "/signup", to: "users#new"
+    get "/contact", to: "static_pages#contact"
     root "static_pages#home"
   end
 end
