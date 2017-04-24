acde606e8c5a654fceb44a2b56675152a09fd36d
Chau Giang <tothelegendary@gmail.com>
2017-04-24 15:49:42 +0700

    Chap 10

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,10 @@ Rails.application.routes.draw do
     get "/about", to: "static_pages#about"
     get "/contact", to: "static_pages#contact"
     get "/signup", to: "users#new"
-    post "/signup", to: "users#create"
     post "/login", to: "sessions#create"
     get "/login", to: "sessions#new"
     delete "/logout", to: "sessions#destroy"
 
-    resources :users, only: :show
+    resources :users, except: :new
   end
 end
