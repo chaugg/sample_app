8ec4981c34ebd940460e24f7d0f55f2e89339718
Chau Giang <tothelegendary@gmail.com>
2017-05-04 08:27:12 +0700

    Chap 13 Pull 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,27 @@
 User.create! name: "chau giang",
-  email: "1337vn@gmail.com",
-  password: "chaugiang26",
-  password_confirmation: "chaugiang26",
+  email: "a@b.c",
+  password: "chaugiang",
+  password_confirmation: "chaugiang",
+  activated: true,
+  activated_at: Time.zone.now,
   admin: true
 
-99.times do |n|
+20.times do |n|
   name = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
+  email = "e-#{n+1}@x.org"
   password = "password"
   User.create! name: name,
     email: email,
     password: password,
-    password_confirmation: password
+    password_confirmation: password,
+    activated: true,
+    activated_at: Time.zone.now
+end
+
+users = User.order(:created_at).take 6
+50.times do
+  content = Faker::Lorem.sentence 5
+  users.each {|user| user.microposts.create! content: content}
 end
 
 users = User.order(:created_at).take 6
